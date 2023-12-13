1cb3812c5dd95f5d0caa3eb52d29525de598c1b9
WaiPhyo <waiphyo@scala-next.com>
2023-12-13 09:50:29 +0630

    Create profile

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('waiphyo');
+    return view('welcome');
 });
 
 Route::namespace('App\Http\Controllers')->group(function () {
